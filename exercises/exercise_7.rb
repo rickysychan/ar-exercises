e8d6902182ad700f6549e2bfb9df7a38e7ecc3e5
Ricky Chan <rickysychan@gmail.com>
2017-10-10 20:35:05 +0000

    all exercises including the bonus completed!

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,22 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts'Please enter a name you want to create a store with'
+
+@suggested_store_name = $stdin.gets.chomp
+
+store = Store.new(name: @suggested_store_name)
+puts store.errors.full_messages unless store.save
+
+
+
+### Exercise 7: Validations for both models
+
+# * BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a 
+# [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods)
+#  - **don't** use a `Validator` class)
+# 2. Ask the user for a store name (store it in a variable)
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, 
+# mens_apparel, and womens_apparel)
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after 
+# you attempt to save/create the record
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,19 @@
 class Store < ActiveRecord::Base
     has_many :employees
+    validates :name, length: {minimum: 3}
+    validates :annual_revenue, numericality: {greater_than: 0}
+
+    validate :must_have_one_of_each,
+ 
+  def must_have_one_of_each
+    unless mens_apparel || womens_apparel
+      errors.add(:mens_apparel, "must have at least one")
+      errors.add(:womens_apparel, "must have at least one")
+    end
+  end
 end
+
+
+# * Stores must always have a name that is a minimum of 3 characters
+# * Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# Stores must carry at least one of the men's or women's apparel
